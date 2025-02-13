b14617fd44ea13e1c08ec0ab23d11e47a7533e02
Asror <asrorrustmaovich007@gmail.com>
2025-02-13 22:26:22 +0500

    swagger added

--- core/settings.py
+++ core/settings.py
@@ -129,10 +129,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'aPYuaARJVXjaVSGcbNBxVcZyxeotLGxG',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '14379',
     }
 }
+
 
 
 # Password validation
